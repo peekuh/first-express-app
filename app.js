d3e95cc2a0efe52f338af3e35e04a54419a3be8c
peek <sar.srijan@gmail.com>
2024-02-22 15:01:12 +0530

    connected mysql database to express

--- app.js
+++ app.js
@@ -6,28 +6,15 @@ const port = 3000
 const {connection} = require("./db/db_config");
 
 app.get('/', (req, res) => {
-    let num1 = parseInt(req.query.num1);
-    let num2 = parseInt(req.query.num2);
-    let opr = req.query.opr;
-    let result;
-
-    switch (opr) {
-        case 'add':
-            result = num1 + num2;
-            break;
-        
-        case 'sub':
-            result = num1 - num2;
-            break;
-        
-        case 'div':
-            result = num1/num2;
-        
-        case 'multiply':
-            result = num1*num2;
-    }
-
-    res.send(`Hello ${result}`)
+    connection.query(`SELECT * FROM movie_data`, (err, results)=>{
+        if (err){
+            console.log(`error in query!`)
+            res.send(`error in db`)
+        }
+        else {
+            res.send(results);
+        }
+    })
 });
 
 app.listen(port, () => {
